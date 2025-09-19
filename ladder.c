0a9334cf7b581997230ef8288039931b91bba63a
indrajit2006 <pawaleindrajit16@gmail.com>
2025-09-19 11:26:58 +0530

    Add files via upload

---
+++ ladder.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks = 75;
+
+    if (marks >= 90)
+    {
+        printf("Grade: A\n");
+    }
+    else if (marks >= 75)
+    {
+        printf("Grade: B\n");
+    }
+    else if (marks >= 50)
+    {
+        printf("Grade: C\n");
+    }
+    else if (marks >= 35)
+    {
+        printf("Grade: D\n");
+    }
+    else
+    {
+        printf("Grade: F\n");
+    }
+    return 0;
+}
